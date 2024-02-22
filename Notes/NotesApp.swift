4a55b42a0224f4623e7837aecfb97712564560f1
soscooter <lelya.rumynin@gmail.com>
2024-02-22 13:27:48 +0300

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by lelya.rumynin@gmail.com on 22.02.24.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
